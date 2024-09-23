01a9e9cc4c1aa4ec0d4cfdb4de887179379b6eb5
Gabriel Alves <gabeelalveskos@gmail.com>
2024-09-23 09:45:34 -0300

    first commit

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'Gabriel',
+    name: 'Gabriel Alves',
+    avatar: 'https://ui-avatars.com/api/?name=Gabriel+Alves&size=100',
+    cover: 'https://www.freecodecamp.org/portuguese/news/content/images/2022/09/Screen-Shot-2019-11-04-at-16.59.21.png',
+    bio: 'Desenvolvedor Pleno',
+    link: 'https://www.google.com/',
+    postCount: 258
+}
